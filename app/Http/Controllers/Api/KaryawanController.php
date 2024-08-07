e769850860a30c60ca1ea8199a61e25ff329469f
Rico <cemilick@gmail.com>
2024-08-07 09:39:37 +0700

    feat: add statistics features on karyawan

--- app/Http/Controllers/Api/KaryawanController.php
+++ app/Http/Controllers/Api/KaryawanController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Karyawan;
+use Illuminate\Http\Request;
 
 class KaryawanController extends BaseCrudController
 {
     protected $class = Karyawan::class;
+
+    public function statistics(Request $request)
+    {
+        $division = $request->input('division');
+
+        $data = Karyawan::select('name');
+        $isActive = Karyawan::selectRaw('*')->where('is_active', 1);
+        $isNonActive = Karyawan::selectRaw('*')->where('is_active', 0);
+
+        if ($division) {
+            $isActive->where('division_id', $division);
+            $isNonActive->where('division_id', $division);
+            $data->where('division_id', $division);
+        }
+
+        $isActive = $isActive->get()->count();
+        $isNonActive = $isNonActive->get()->count();
+        $data = $data->get();
+
+        return $this->sendResponse([
+            'total_karyawan' => $isActive + $isNonActive,
+            'total_karyawan_active' => $isActive,
+            'total_karyawan_non_active' => $isNonActive,
+            'data' => $data
+        ], 'Data return successfully.');
+    }
 }
